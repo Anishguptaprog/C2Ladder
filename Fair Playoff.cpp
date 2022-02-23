045f3541a6dbfd35221f727fae26f3963590bcb2
Anishguptaprog <81504815+Anishguptaprog@users.noreply.github.com>
2022-02-23 16:41:14 +0530

    Update Fair Playoff.cpp

--- Fair Playoff.cpp
+++ Fair Playoff.cpp
@@ -5,24 +5,14 @@ int main()
 	cin>>t;
 	while(t--)
 	{	int s[4];
-		int a[2]={0};
+		
 		for(int i=0;i<4;i++)
 		{	cin>>s[i];
 		}
-		if(s[0]>s[1])
-			a[0]=s[0];
-		else 
-			a[0]=s[1];
-		if(s[2]>s[3])
-			a[1]=s[2];
-		else 
-			a[1]=s[3];
-		
-		sort(a,a+2);
-		sort(s,s+4);
-		if(a[0]==s[2] && a[1]==s[3])
-			cout<<"Yes"<<endl;
-		else 
-			cout<<"NO"<<endl;
+		if (min(s[0], s[1]) > max(s[2], s[3]) || max(s[0], s[1]) < min(s[2], s[3]))
+      cout << "NO"<<endl;
+    else
+      cout << "YES"<<endl;
 	}
+	return 0;
 }
